ed478cc637fc8de06c12de14297337359a5af3dd
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-09-13 21:23:44 +0530

    added hosting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <div className='App'>Hello Mars! Can thankyou review this?</div>
+  return (
+    <div className='App'>
+      Hello Mars! Can thankyou review this?
+      <br />
+      This is awesome!
+      <br />
+      This deploy happened through github actions!
+    </div>
+  )
 }
 
 export default App
+
+// firebase deploy --only hosting:epicgames-dev-iamkarthick
+
+// "site": "epicgames-dev-iamkarthick",
